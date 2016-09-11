10853d52f388da4bdf42df87a049fce489531b14
A_Chang <441642165@qq.com>
2016-09-11 13:59:08 +0800

    JavaNet 2nd

--- src/ip/TcpClient.java
+++ src/ip/TcpClient.java
@@ -10,7 +10,7 @@ package ip;
 
 		public static void main(String[] args) throws UnknownHostException, IOException {
 			// TODO 自动生成的方法存根
-	Socket s=new Socket("10.90.95.50",10007);
+	Socket s=new Socket("10.90.95.50",10007);//创建码头
 	OutputStream out=s.getOutputStream();
 	out.write("tcp I'm coming".getBytes());
 	s.close();
